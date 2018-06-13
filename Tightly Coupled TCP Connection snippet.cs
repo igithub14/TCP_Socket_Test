5d35976989230c07f6f051fc0e9323e2f3813911
igithub14 <igithub14@users.noreply.github.com>
2018-06-13 12:55:53 +0200

    Update Tightly Coupled TCP Connection snippet.cs

--- Tightly Coupled TCP Connection snippet.cs
+++ Tightly Coupled TCP Connection snippet.cs
@@ -1,2 +1,8 @@
 
 //...
+//This is a C# code snippet.
+
+/*
+  This snippet depicts how Client can invoke a remote function via a TCP conneciton.
+  This procedure is reasonable on a tightly coupled system.
+*/
