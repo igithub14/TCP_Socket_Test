d5163e2a0158c09c034e73f49f721bde276abe15
igithub14 <igithub14@users.noreply.github.com>
2018-06-13 16:23:58 +0200

    Update tcp_server.c

--- tcp_server.c
+++ tcp_server.c
@@ -1,6 +1,6 @@
 
 //Run it on a Linux System.
-//Put it inside: "~/Sockets/TCPClient" directory.
+//Put it inside: "~/Sockets/TCPServer" directory.
 
 /*
  Here is depicted the workflow for the TCP client:
@@ -24,16 +24,34 @@
              to the other socket he's connected to
 */ 
 
-//We want to include some stuff from stdio and stdlib
 #include <stdio.h>
 #include <stdlib.h>
 
-// The following are for definition of socket functions and API
 #include <sys/types.h>
 #include <sys/socket.h>
 
-//... for the structures we need to store address infos
 #include <netinet/in.h>
 
 int main () {
+ 
+ //define a strring to hold the data to be sent to the client
+ char server_message[256] = "You have rached the server";
+ 
+ //cresate the server socket
+ int server_socket;
+ server_socket = socket(AF_INET, SOCK_STREAM, 0);
+ 
+ // define the server address
+ struct sockaddr_in_server_address;
+ server_adress.sin.family = AF_INET;
+ server_adress.sin_port = htons(9002);
+ server_adress.sin_addr.s_addr = INADDR_ANY;
+ 
+ //bind the socket to our specified IP and port
+ bind (server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
+ 
+ listen(server_socket, 5);
+ 
+ return 0;
+}
     
