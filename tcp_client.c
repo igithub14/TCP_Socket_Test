383b2fb0b7d93ecdd7d48475e7af0d00b4efbaaf
igithub14 <igithub14@users.noreply.github.com>
2018-06-13 11:25:09 +0200

    Update tcp_client.c

--- tcp_client.c
+++ tcp_client.c
@@ -1,3 +1,11 @@
 //Run it on a Linux System.
 //Put it inside: "~/Sockets/TCPClient" directory.
 
+//We want to include some stuff from stdio and stdlib
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/type.h>
+
+int main () {
+}
