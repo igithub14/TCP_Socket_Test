9828007e17498351ad37066942fc882780baf278
igithub14 <igithub14@users.noreply.github.com>
2018-06-14 06:34:25 +0000

    Update Tightly Coupled TCP Connection snippet.cs

--- Tightly Coupled TCP Connection snippet.cs
+++ Tightly Coupled TCP Connection snippet.cs
@@ -7,3 +7,17 @@
   This procedure is only reasonable on a tightly coupled system.
 */
 
+//...
+String hostName = "www.eaipatterns.com"; int port = 80; 
+ 
+IPHostEntry hostInfo = Dns.GetHostByName(hostName); IPAddress address = hostInfo.AddressList[0]; 
+ 
+IPEndPoint endpoint = new IPEndPoint(address, port); 
+ 
+Socket socket = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp); socket.Connect(endpoint); 
+ 
+byte[] amount = BitConverter.GetBytes(1000); byte[] name   = Encoding.ASCII.GetBytes("Joe");
+
+int bytesSent = socket.Send(amount); bytesSent    += socket.Send(name); 
+ 
+socket.Close(); 
