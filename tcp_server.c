3998e00a7e65c2db9037aea3fc8fd07aac7585d9
igithub14 <igithub14@users.noreply.github.com>
2018-06-14 21:00:46 +0200

    Update tcp_server.c

--- tcp_server.c
+++ tcp_server.c
@@ -11,7 +11,7 @@
  |               |
  +---------------+
          |
-         v
+         V
  +---------------+
  |               |
  |    bind()     |   Bind the socket to an IP and Port through bind()
