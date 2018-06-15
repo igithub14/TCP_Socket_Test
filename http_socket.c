08b0ae1a022c037b7b4d9e4bef42276f0800c241
igithub14 <igithub14@users.noreply.github.com>
2018-06-15 16:04:56 +0200

    Update http_socket.c

--- http_socket.c
+++ http_socket.c
@@ -9,12 +9,26 @@ Inside the Sokerts folder, we create the file: http:socnet.c
 #include <string.types.h>
 
 int main () {
+
+  // we whant to render a XML file.
+
+  //we are gona use a c method for opening and reading a file.
   
-  retourn 0;
+  // open a ifle to serve
+  FILE *html_data;
+  html_data = fopen("index.html", "r");
+  
+  char response_data[1024];
+  fgets(response_data, 1024, html_data);
+  
+  char http_header[1024] = "HTTP/1.1 200 OK\r\n\n"
+  strcat(http_header. response_data);
+  
+  // create a socket
+  int server_socket;
+  server_socket = (AF_INET, SOCK_STREAM, 0);
+  
+  
+  //...
+  return 0;
 }
-
-// we whant to render a XML file.
-
-//we are gona use a c method for opening and reading a file.
-
-///...
