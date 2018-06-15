95dbc8689ff392fa39e079ee57ecbcaca731b845
igithub14 <igithub14@users.noreply.github.com>
2018-06-15 15:45:42 +0200

    Create http_socket.c

---
+++ http_socket.c
@@ -0,0 +1,20 @@
+
+Inside the Sokerts folder, we create the file: http:socnet.c
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <string.types.h>
+
+int main () {
+  
+  retourn 0;
+}
+
+// we whant to render a XML file.
+
+//we are gona use a c method for opening and reading a file.
+
+///...
